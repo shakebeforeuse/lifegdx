7eb6c6e8b29e0fb370d58ea796cb04c3c4fee728
Manuel F. <shakebeforeuse@gmail.com>
2014-12-05 13:33:28 +0100

    Added game mechanics and random-generated intial state. LibGDX working.

---
+++ Lifegdx-core/src/es/mpitch/life/Life.java
@@ -0,0 +1,182 @@
+package es.mpitch.life;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.BrokenBarrierException;
+
+public class Life implements Runnable
+{
+	private static boolean[][] grid;
+	private static boolean[][] nextGrid;
+	private static int size;
+	private static int cores = Runtime.getRuntime().availableProcessors();
+	private static ExecutorService threadPool = Executors.newFixedThreadPool(cores);
+	private static CyclicBarrier barrier = new CyclicBarrier(cores + 1);
+	private int beginX;
+	private int endX;
+	
+	public void randomGrid(int size)
+	{
+		Life.size = size;
+		grid = new boolean[size][size];
+		nextGrid = new boolean[size][size];
+		
+		for (int i = 0; i < size; ++i)
+			for (int j = 0; j < size; ++j)
+				grid[j][i] = Math.random() < 0.1;
+		
+		// //Glider
+		// grid[155][155] = true;
+		// grid[156][155] = true;
+		// grid[157][155] = true;
+		// grid[155][156] = true;
+		// grid[156][157] = true;
+		// //Glider
+		// grid[355][355] = true;
+		// grid[356][355] = true;
+		// grid[357][355] = true;
+		// grid[355][356] = true;
+		// grid[356][357] = true;
+		// //Blinker
+		// grid[200][200] = true;
+		// grid[200][201] = true;
+		// grid[200][202] = true;
+	}
+	
+	public Life(int size)
+	{
+		if (grid == null)
+			randomGrid(size);
+			
+		this.beginX = 0;
+		this.endX   = size;
+	}
+	
+	public Life(int size, int beginX, int endX)
+	{
+		if (grid == null)
+			randomGrid(size);
+			
+		this.beginX = beginX;
+		this.endX   = endX;
+	}
+	
+	private int mod(int a, int b)
+	{
+		int r = a % b;
+		if (r < 0)
+		{
+			r += b;
+		}
+		
+		return r;
+	}
+	
+	public int aliveNeighbours(int x, int y)
+	{
+		int alives = 0;
+		
+		for (int i = -1; i <= 1; ++i)
+		{
+			for (int j = -1; j <= 1; ++j)
+			{
+				int col = mod(y+j, size);
+				int row = mod(x+i, size);
+				
+				if (grid[col][row] && (col != y || row != x))
+					++alives;
+			}
+		}
+
+		return alives;
+	}
+	
+	public boolean nextState(int x, int y)
+	{
+		boolean newState = false;
+		int neighbours = aliveNeighbours(x, y);
+		
+		if (grid[y][x])
+		{
+			if (neighbours == 2 || neighbours == 3)
+				newState = true;
+			else
+				newState = false;
+		}
+		else
+			if (neighbours == 3)
+				newState = true;
+			
+		return newState;
+	}
+	
+	public void nextSubGeneration(int beginX, int endX, int beginY, int endY)
+	{
+		for (int i = beginX; i < endX; ++i)
+			for (int j = beginY; j < endY; ++j)
+				nextGrid[j][i] = nextState(i, j);
+	}
+	
+	public static void nextGeneration()
+	{
+		int parts = size/cores;
+		
+		nextGrid = new boolean[size][size];
+		
+		for (int i = 0; i < cores; ++i)
+		{
+			int beginX = i*parts;
+			int endX;
+			
+			if (i+1 == cores)
+				endX = size;
+			else
+				endX = (i+1)*parts;
+			
+			threadPool.execute(new Life(size, beginX, endX));
+		}
+		
+		try
+		{
+			barrier.await();
+			barrier.reset();
+			acceptNextGeneration();
+		}
+		catch (InterruptedException e)
+		{
+			System.out.println("Error: " + e.getMessage());
+		}
+		catch (BrokenBarrierException e)
+		{
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+	
+	public void run()
+	{
+		nextSubGeneration(beginX, endX, 0, size);
+		try
+		{
+			barrier.await();
+		}
+		catch (InterruptedException e)
+		{
+			System.out.println("Error: " + e.getMessage());
+		}
+		catch (BrokenBarrierException e)
+		{
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+	
+	public boolean[][] getGrid()
+	{
+		return grid;
+	}
+	
+	public static void acceptNextGeneration()
+	{
+		grid = nextGrid;
+	}
+}
--- Lifegdx-core/src/es/mpitch/life/Lifegdx.java
+++ Lifegdx-core/src/es/mpitch/life/Lifegdx.java
@@ -2,26 +2,72 @@ package es.mpitch.life;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Lifegdx extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	
+	ShapeRenderer shapes;
+	Life life;
+	int size;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		shapes = new ShapeRenderer();
+		size = Math.min(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		life = new Life(size);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		
+		boolean[][] grid = life.getGrid();
+		
+		shapes.begin(ShapeType.Filled);
+		
+		for (int i=0; i<size; ++i)
+		{
+			for (int j=0; j<size; ++j)
+			{
+				Color color = (grid[j][i]? Color.WHITE : Color.BLACK);
+				shapes.rect(i, j, 1, 1, color, color, color, color);
+			}
+		}
+		
+		shapes.end();
+		
+		Life.nextGeneration();
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		super.resize(width, height);
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		super.pause();
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		super.resume();
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		super.dispose();
+		shapes.dispose();
 	}
 }
